418643df8afc60b10a986208ee3529862a49f425
paul <paul@mdbd.me>
2022-06-12 23:13:32 +0900

    build: 의존성 복잡도가 올라가서 그냥 fat-jar로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.jvm.tasks.Jar
 
 plugins {
     kotlin("jvm") version "1.6.21"
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    `java-library`
 }
 
 
@@ -18,12 +21,14 @@ repositories {
     mavenCentral()
 }
 
+
+
 dependencies {
-    implementation("io.projectreactor.netty:reactor-netty:1.0.19")
-    implementation("org.springframework:spring-webflux:5.3.20")
-    implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+    api("io.projectreactor.netty:reactor-netty:1.0.19")
+    api("org.springframework:spring-webflux:5.3.20")
+    api("com.fasterxml.jackson.core:jackson-core:2.13.3")
+    api("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    api("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
 
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
@@ -32,6 +37,24 @@ tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "11"
+    }
+}
+
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
+//        archiveClassifier.set("standalone") // Naming the jar
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+
+    build {
+        dependsOn(fatJar) // Trigger fat jar creation during build
     }
 }
 
