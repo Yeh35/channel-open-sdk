d298dffb3f8fd87e175f095eabbacfb2b6e191e4
paul <paul@mdbd.me>
2022-06-12 21:06:16 +0900

    feat: spring boot 프로젝트에서 lib 프로젝트로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.0"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
--- src/main/kotlin/io/github/yeh35/channelopenapi/ChannelOpenApiApplication.kt
+++
@@ -1,8 +0,0 @@
-package io.github.yeh35.channelopenapi
-
-
-class ChannelOpenApiApplication
-
-fun main(args: Array<String>) {
-
-}
