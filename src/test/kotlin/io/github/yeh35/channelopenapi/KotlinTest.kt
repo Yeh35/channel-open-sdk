c2fa228ef3cbcd64b6d9aa440a8732f0880f4354
Sang oh yeh <46643699+Yeh35@users.noreply.github.com>
2022-06-15 22:54:18 +0900

    test : 코드 정리

--- src/test/kotlin/io/github/yeh35/channelopenapi/KotlinTest.kt
+++ src/test/kotlin/io/github/yeh35/channelopenapi/KotlinTest.kt
@@ -17,69 +17,6 @@ import kotlin.streams.toList
 class KotlinTest {
 
     @Test
-    fun testColor() {
-        println(Color.RED.run { String.format("#%02x%02x%02x", red, green, blue) })
-    }
-
-    @Test
-    fun testBlock() {
-        val blocks = listOf<Block>(
-            TextBlock("안녕"),
-            CodeBlock("println(hello)"),
-            BulletBlock(
-                "bullet", blocks = listOf(
-                    TextBlock("bullet: 안녕1"),
-                    TextBlock("bullet: 안녕2"),
-                )
-            )
-        )
-
-        val objectMapper = ObjectMapper()
-        println(objectMapper.writeValueAsString(blocks))
-    }
-
-    @Test
-    fun testMapper() {
-        val json = "{\n" +
-                "    \"id\": \"218869\",\n" +
-                "    \"channelId\": \"92517\",\n" +
-                "    \"accountId\": \"156100\",\n" +
-                "    \"username\": \"jaws0910\",\n" +
-                "    \"name\": \"예상오\",\n" +
-                "    \"showDescriptionToFront\": false,\n" +
-                "    \"email\": \"jaws0910@gmail.com\",\n" +
-                "    \"showEmailToFront\": false,\n" +
-                "    \"mobileNumber\": \"+821077646003\",\n" +
-                "    \"showMobileNumberToFront\": false,\n" +
-                "    \"role\": \"owner\",\n" +
-                "    \"removed\": false,\n" +
-                "    \"createdAt\": 1653917436966,\n" +
-                "    \"displayAsChannel\": false,\n" +
-                "    \"defaultGroupWatch\": \"all\",\n" +
-                "    \"defaultDirectChatWatch\": \"all\",\n" +
-                "    \"defaultUserChatWatch\": \"all\",\n" +
-                "    \"chatAlertSound\": \"woody\",\n" +
-                "    \"operatorScore\": 0.0,\n" +
-                "    \"touchScore\": 17.189999,\n" +
-                "    \"operatorEmailReminder\": true,\n" +
-                "    \"operator\": true,\n" +
-                "    \"operatorScheduling\": false,\n" +
-                "    \"avatarUrl\": \"https://cf.channel.io/avatar/emoji/koala.f392e0.png\",\n" +
-                "    \"avatarUrl\": \"https://cf.channel.io/avatar/emoji/koala.f392e0.png\",\n" +
-                "    \"managerId\": \"218869\"\n" +
-                "}"
-
-        val objectMapper = ObjectMapper().registerModule(
-            KotlinModule.Builder()
-                .configure(KotlinFeature.NullIsSameAsDefault, true)
-                .configure(KotlinFeature.NullToEmptyCollection, true)
-                .configure(KotlinFeature.NullToEmptyMap, true)
-                .build()
-        )
-        println(objectMapper.readValue(json, Manager::class.java))
-    }
-
-    @Test
     fun testTest() {
         val xAccessKey = ""
         val xAccessSecret = ""
@@ -194,25 +131,6 @@ class KotlinTest {
 
         for (webhook in webhookAll) {
 
-            if (webhook.name == "TestCode") {
-                // Webhook 삭제하기
-                val isDelete = channel.deleteWebhook(webhook)
-                println("delete WebHook: $isDelete")
-            }
-        }
-    }
-
-    @Test
-    fun delete() {
-        val xAccessKey = "62a58d04644cae7732be"
-        val xAccessSecret = "2fa881a786cd3c4e9ac886ee74fb6004"
-        val channel = Channel.create(xAccessKey, xAccessSecret)
-
-        // 등록된 Webhook 가져오기
-        val webhookAll = channel.getWebhookAll()
-        println("webhookAll Size: ${webhookAll.size}")
-
-        for (webhook in webhookAll) {
             if (webhook.name == "TestCode") {
                 // Webhook 삭제하기
                 val isDelete = channel.deleteWebhook(webhook)
